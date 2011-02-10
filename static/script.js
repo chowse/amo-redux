9e2abf52f0976b4f37e9eed758af616d1b79c518
Chris Howse <chowse@mozilla.com>
2011-02-09 17:36:07 -0800

    Added toggles for certain states and features

--- static/script.js
+++ static/script.js
@@ -7,6 +7,22 @@
 			$(this).closest('.expandable').toggleClass('expanded collapsed');
 			return false;
 		});
+
+		$('#proto-controls .close').click(function() {
+			$('#proto-controls').hide();
+			return false;
+		});
 		
+		$('#proto-controls').delegate('a', 'click', function() {
+			var btn = $(this),
+			    cls = btn.data('class'),
+			    doc = $(document.documentElement),
+			    enabled = doc.hasClass(cls);
+			
+			doc.toggleClass(cls, !enabled);
+			btn.toggleClass('selected', !enabled);
+			
+			return false;
+		})
 	});
 })(jQuery);
